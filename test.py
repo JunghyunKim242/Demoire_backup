a72b7055552fe3f92f81a7770e772c60af8c8c16
JunghyunKim242 <81853589+JunghyunKim242@users.noreply.github.com>
2021-09-24 20:40:18 +0900

    Update test.py

--- test.py
+++ test.py
@@ -6,36 +6,32 @@ import time
 import numpy as np
 import torch
 from torch import nn
-from torch.nn import MSELoss, L1Loss
+# from torch.nn import MSELoss, L1Loss
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from torchvision import transforms
 import matplotlib
 import matplotlib.pyplot as plt
-
-from Util.util_collections import tensor2im, save_single_image
-from Net.LossNet import L2_LOSS
-from dataset.dataset import Moire_dataset
+from Util.util_collections import tensor2im, save_single_image, PSNR
+from dataset.dataset import Moire_dataset, AIMMoire_dataset
 from torchnet import meter
 import colour
-from skimage import metrics
 import time
+from skimage import metrics
 
-# @torch.no_grad()
+
 def test(args, model):
-    os.environ["CUDA_VISIBLE_DEVICES"] = '3,2,1,0'
+    os.environ["CUDA_VISIBLE_DEVICES"] = '3,2'
     args.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    if not os.path.exists(args.save_prefix):
+        os.makedirs(args.save_prefix)
+
     print('torch devices = ', args.device)
     print('save_path = ', args.save_prefix)
 
-    if not os.path.exists(args.save_prefix):        os.makedirs(args.save_prefix)
-    print(args.save_prefix)
-
-    image_testset_path_demoire = "{0}/Testset_Result/".format(args.save_prefix)
-    if not os.path.exists(image_testset_path_demoire): os.makedirs(image_testset_path_demoire)
-
-    Moiredata_test = Moire_dataset(args.testdata_path)
+    Moiredata_test = AIMMoire_dataset(args.testmode_path)
     test_dataloader = DataLoader(Moiredata_test,
                                   batch_size=args.batchsize,
                                   shuffle=True,
@@ -46,22 +42,28 @@ def test(args, model):
     model = model.to(args.device)
     model.eval()
 
-    L2_loss = L2_LOSS()
-    psnr_meter = meter.AverageValueMeter()
-    Loss_meter = meter.AverageValueMeter()
+    psnr_output_meter = meter.AverageValueMeter()
+    psnr_input_meter = meter.AverageValueMeter()
+
+    image_train_path_moire = "{0}/{1}".format(args.save_prefix, "Moirefolder")
+    image_train_path_clean = "{0}/{1}".format(args.save_prefix, "Cleanfolder")
+    image_train_path_demoire = "{0}/{1}".format(args.save_prefix, "Demoirefolder")
+
+    if not os.path.exists(image_train_path_moire):      os.makedirs(image_train_path_moire)
+    if not os.path.exists(image_train_path_clean):      os.makedirs(image_train_path_clean)
+    if not os.path.exists(image_train_path_demoire):    os.makedirs(image_train_path_demoire)
 
     if args.pretrained_path:
         checkpoint = torch.load(args.pretrained_path)
         model.load_state_dict(checkpoint['model'])
+        last_epoch = checkpoint["epoch"]
+        # optimizer_state = checkpoint["optimizer"]
+        # optimizer.load_state_dict(optimizer_state)
 
-
-    for  ii, (moires, clears, labels) in tqdm(enumerate(test_dataloader)):
+    for ii ,(moires,clears,labels) in tqdm(enumerate(test_dataloader)):
         moires = moires.to(args.device)
         clears = clears.to(args.device) #batch,1,256,256(unet,moire)
         outputs = model(moires) # 32,1,256,256 = 32,1,256,256
-
-        loss = L2_loss(outputs, clears)
-        Loss_meter.add(loss.item())
 
         moires = tensor2im(moires)
         outputs = tensor2im(outputs)
@@ -69,75 +71,21 @@ def test(args, model):
 
         bs = moires.shape[0]
         for jj in range(bs):
-            output, clear, moire, label = outputs[jj], clears[jj], moires[jj], labels[jj]
+            moire, clear, label, output = moires[jj], clears[jj], labels[jj], outputs[jj]
 
-            # psnr = colour.utilities.metric_psnr(output, clear)
-            psnr = metrics.peak_signal_noise_ratio(output, clear)
-            psnr_meter.add(psnr)
-            psnr_in_gt = metrics.peak_signal_noise_ratio(moire, clear)
+            psnr_output = metrics.peak_signal_noise_ratio(output,clear)
+            psnr_output_meter.add(psnr_output)
 
-            img_path = "{0}/_Demoire_{1}_PSNR:{2:.4f}_.png".format(image_testset_path_demoire, label, psnr)
-            save_single_image(output, img_path)
+            psnr_input = metrics.peak_signal_noise_ratio(moire,clear)
+            psnr_input_meter.add(psnr_input)
 
-            img_path2 = "{0}/_Moire_{1}_PSNR:{2:.4f}_.png".format(image_testset_path_demoire, label, psnr_in_gt)
-            save_single_image(moire, img_path2)
+            img_path1 = "{0}/{1}_moire_{2:.4f}.png".format(image_train_path_moire, label, psnr_input)
+            save_single_image(moire, img_path1)
+            img_path2 = "{0}/{1}_clean.png".format(image_train_path_clean, label)
+            save_single_image(clear, img_path2)
+            img_path3 = "{0}/{1}_demoire_{2:.4f}.png".format(image_train_path_demoire, label, psnr_output)
+            save_single_image(output, img_path3)
 
-            img_path3 = "{0}/Clear_{1}_.png".format(image_testset_path_demoire, label)
-            save_single_image(clear, img_path3)
-
-
-    with open(args.save_prefix + "PSNR and Loss Testset.txt", 'w') as f:
-        f.write("PSNR: {:}\nLoss{:} = ".format( psnr_meter.value()[0], Loss_meter.value()[0] ))
+    print('TESTdata PSNR = ',psnr_output_meter.value()[0])
 
 
-
-
-# with torch.no_grad():
-#     def val(model, dataloader,epoch,args, vis=None, train_sample = False): # 맨처음 확인할때의 epoch == -1
-#         model.eval()
-#         criterion_l2 = L2_LOSS()
-#
-#         loss_meter = meter.AverageValueMeter()
-#         psnr_output_meter = meter.AverageValueMeter()
-#
-#
-#         loss_meter.reset()
-#         psnr_output_meter.reset()
-#
-#         for ii, (val_moires, val_clears, labels) in tqdm(enumerate(dataloader)):
-#             val_moires = val_moires.to(args.device)
-#             val_clears = val_clears.to(args.device)
-#
-#
-#             val_outputs = model(val_moires)
-#
-#             loss1 = criterion_l2(val_outputs, val_clears)
-#             loss_meter.add(loss1.item())
-#
-#
-#
-#             val_moires = tensor2im(val_moires) # type tensor to numpy .detach().cpu().float().numpy()
-#             val_outputs = tensor2im(val_outputs)
-#             val_clears = tensor2im(val_clears)
-#
-#
-#             bs = val_moires.shape[0]
-#                 for jj in range(bs):
-#                     output, clear, moire = val_outputs[jj], val_clears[jj], val_moires[jj]
-#                     label = labels[jj]
-#
-#                     psnr_output_individual = colour.utilities.metric_psnr(output, clear)
-#                     psnr_output_meter.add(psnr_output_individual)
-#
-#
-#                         img_path = "{0}/{1}_epoch:{2:04d}_PSNR:{3:.4f}_demoire.png".format(image_train_path_demoire, label,epoch+1 ,psnr_output_individual)
-#                         save_single_image(output, img_path)
-#
-#                         psnr_in_gt = colour.utilities.metric_psnr(moire, clear)
-#                         img_path2 = "{0}/{1}_{2:.4f}_moire.png".format( image_train_path_moire, label, psnr_in_gt)
-#                         img_path3 = "{0}/{1}_clean.png".format(         image_train_path_clean, label)
-#                         save_single_image(moire, img_path2)
-#                         save_single_image(clear, img_path3)
-#
-#         return loss_meter.value()[0]
-#
